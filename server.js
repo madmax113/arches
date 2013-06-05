afdcf6a4024cea555710c6fc69cf5ff77cac0df6
Meghan Desai <meghan.desai@gmail.com>
2013-06-04 23:29:36 -0700

    modified serve

--- server.js
+++ server.js
@@ -32,10 +32,5 @@ http.createServer(function(request, response) {
       
       
    }) ;
-}).listen(process.env.PORT, process.env.IP);
+}).listen(process.env.PORT);
 
-// Note: when spawning a server on Cloud9 IDE, 
-// listen on the process.env.PORT and process.env.IP environment variables
-
-// Click the 'Run' button at the top to start your server,
-// then click the URL that is emitted to the Output tab of the console

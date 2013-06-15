cd8390969a075ba5e284dc8160854906ef8c4212
Meghan Desai <meghan.desai@gmail.com>
2013-06-14 20:34:42 -0700

    trying express

--- server.js
+++ server.js
@@ -1,36 +1,12 @@
-//Project Arches.
-//HTTP Server 
-//Author: meghan.desai@gmail.com
-// reference the http module so we can create a webserver
-var http = require("http");
-var url = require("url");
-var router = require("./router.js");
-var clientResp = require('./clientResp.js');
+var express = require("express");
+var app = express();
+app.use(express.logger());
 
-// create a server
-http.createServer(function(request, response) {
-// Attach listener
- request.on('end', function () { 
-     
-     
-     var path = url.parse(request.url,true).pathname;
-     
-     if(path=='/input/'){
-     
-      // Parse the request for arguments and store them in _get variable. 
-      // This function parses the url from request and returns object representation. 
-      var _get = url.parse(request.url, true).query; 
-      // Write headers to the response. 
-      response.writeHead(200, { 
-         'Content-Type': 'text/plain'
-      }); 
-      // Send data and end response. 
-      response.end("Project Arches Prototype\n" + clientResp.message(_get['data'])); }
-      else{
-          response.end("Project Arches Prototype\nNo Parameters Detected!");
-      }
-      
-      
-   }) ;
-}).listen(process.env.PORT);
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
 
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});

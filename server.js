4406df19177c5f8fd9b87b866b4743e9768422dd
Meghan Desai <meghan.desai@gmail.com>
2013-06-14 21:14:46 -0700

    express params try 2

--- server.js
+++ server.js
@@ -2,16 +2,18 @@
 //HTTP Server 
 //Author: meghan.desai@gmail.com
 // reference the http module so we can create a webserver
-var http = require("http");
-var url = require("url");
-
 var express = require("express");
 var app = express();
 app.use(express.logger());
+var intro = 'Welcome to the Project Arches Prototype!';
 
 app.get('/', function(request, response) {
-  var path = url.parse(request.url,true).pathname;
-  response.send('Welcome to the Project Arches Prototype! Your path is '+path);
+  response.send(intro + 'You provided no paramaters!');
+});
+
+
+app.get('/input/', function(req, res){
+  res.send(intro + 'Your paramater is: ' + req.param('data'));
 });
 
 var port = process.env.PORT || 5000;
@@ -20,19 +22,3 @@ app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
-
-    
-     //var path = url.parse(request.url,true).pathname;
-     
-     //if(path=='/input/'){
-     
-      // Parse the request for arguments and store them in _get variable. 
-      // This function parses the url from request and returns object representation. 
-     // var _get = url.parse(request.url, true).query; 
-      // Write headers to the response. 
-      //response.writeHead(200, { 
-        // 'Content-Type': 'text/plain'
-      //}); 
-      // Send data and end response. 
-     // response.end("Project Arches Prototype\n" + clientResp.message(_get['data'])); }
-   

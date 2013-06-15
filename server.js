f1def313439e145dca57b9b68d56b15a5389c9e9
Meghan Desai <meghan.desai@gmail.com>
2013-06-14 20:57:16 -0700

    back to express

--- server.js
+++ server.js
@@ -4,33 +4,35 @@
 // reference the http module so we can create a webserver
 var http = require("http");
 var url = require("url");
-var router = require("./router.js");
-var clientResp = require('./clientResp.js');
 
-// create a server
-http.createServer(function(request, response) {
-// Attach listener
- request.on('end', function () { 
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  var path = url.parse(request.url,true).pathname;
+  response.send('Welcome to the Project Arches Prototype!\n'+path);
+});
+
+var port = process.env.PORT || 5000;
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+
+    
+     //var path = url.parse(request.url,true).pathname;
      
-     
-     var path = url.parse(request.url,true).pathname;
-     
-     if(path=='/input/'){
+     //if(path=='/input/'){
      
       // Parse the request for arguments and store them in _get variable. 
       // This function parses the url from request and returns object representation. 
-      var _get = url.parse(request.url, true).query; 
+     // var _get = url.parse(request.url, true).query; 
       // Write headers to the response. 
-      response.writeHead(200, { 
-         'Content-Type': 'text/plain'
-      }); 
+      //response.writeHead(200, { 
+        // 'Content-Type': 'text/plain'
+      //}); 
       // Send data and end response. 
-      response.end("Project Arches Prototype\n" + clientResp.message(_get['data'])); }
-      else{
-          response.end("Project Arches Prototype\nNo Parameters Detected!");
-      }
-      
-      
-   }) ;
-}).listen(process.env.PORT||5000);
-
+     // response.end("Project Arches Prototype\n" + clientResp.message(_get['data'])); }
+   
